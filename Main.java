cdd42984fb2fc03cf38a7b89dbe2faee653cb539
Esteban Montes <estebanmontes@Estebans-MacBook-Pro.local>
2023-10-26 20:51:52 +0200

    Otra vez

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(" HOLAAAA ");
+        System.out.println(" No se \ncomo \nponer el ignore ");
     }
 }
